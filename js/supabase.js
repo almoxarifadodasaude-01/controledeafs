a287a7ada7322cdc081dd0421cfa6a33054f2587
almoxarifadodasaude-01 <almoxarifadodasaude4@gmail.com>
2026-08-20 14:21:58 -0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -2,8 +2,8 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
 
 // Cole aqui as chaves do seu projeto oficial do Almoxarifado
-const supabaseUrl = 'SUA_SUPABASE_URL_AQUI'
-const supabaseAnonKey = 'SUA_SUPABASE_ANON_KEY_AQUI'
+const supabaseUrl = 'https://ufzbrvmvtmywxmjfhcqf.supabase.co/rest/v1/'
+const supabaseAnonKey = 'sb_publishable_gSno2CLaM-LuPwFehJ8sFw_BuX1hswF'
 
 // Inicializa e exporta o cliente para ser usado nos outros arquivos
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
